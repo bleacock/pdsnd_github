b4df20d82e405735af64704758fc7cc9c2f7c6df
Brian Leacock <bleacock@me.com>
2019-01-13 13:03:31 -0600

    Misc changes to raw input of bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -226,7 +226,7 @@ def main():
 
 
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart? Please enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
